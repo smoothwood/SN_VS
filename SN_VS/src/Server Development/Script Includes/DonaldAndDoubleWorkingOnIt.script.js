4486c520c8814876832dc474f3ad1741f650663d
smoothwood <ycm@outlook.com>
2022-11-02 20:31:40 +0800

    Worked on feature_branch1

---
+++ SN_VS/src/Server Development/Script Includes/DonaldAndDoubleWorkingOnIt.script.js
@@ -0,0 +1,8 @@
+var DonaldAndDoubleWorkingOnIt = Class.create();
+DonaldAndDoubleWorkingOnIt.prototype = {
+	initialize: function () {
+		// Donald added comments for feature_branch1 in new file
+	},
+
+	type: "DonaldAndDoubleWorkingOnIt"
+};
--- SN_VS/src/Server Development/Script Includes/MultiDeveloperWillWorkOnIt.script.js
+++ SN_VS/src/Server Development/Script Includes/MultiDeveloperWillWorkOnIt.script.js
@@ -2,6 +2,7 @@ var MultiDeveloperWillWorkOnIt = Class.create();
 MultiDeveloperWillWorkOnIt.prototype = {
 	initialize: function () {
 		// my comments
+		// Donald added comments for feature_branch1
 	},
 
 	type: "MultiDeveloperWillWorkOnIt"
