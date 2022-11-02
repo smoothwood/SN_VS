a9a122a7cca4618ff330017c37ad7228c54b828f
shuangjianyang <shuangjian.yang@ray-soft.com>
2022-11-02 21:02:53 +0800

    test

--- SN_VS/src/Server Development/Script Includes/DonaldAndDoubleWorkingOnIt.script.js
+++ SN_VS/src/Server Development/Script Includes/DonaldAndDoubleWorkingOnIt.script.js
@@ -2,6 +2,7 @@ var DonaldAndDoubleWorkingOnIt = Class.create();
 DonaldAndDoubleWorkingOnIt.prototype = {
 	initialize: function () {
 		// Donald added comments for feature_branch1 in new file
+		// Double added comments for feature_branch1 in new file
 	},
 
 	type: "DonaldAndDoubleWorkingOnIt"
