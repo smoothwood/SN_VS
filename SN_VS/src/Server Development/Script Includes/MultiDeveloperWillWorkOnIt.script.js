12430112b3213da82eaa91bfe6a015d594c04e0c
shuangjianyang <shuangjian.yang@ray-soft.com>
2022-11-02 20:33:40 +0800

    double add

--- SN_VS/src/Server Development/Script Includes/MultiDeveloperWillWorkOnIt.script.js
+++ SN_VS/src/Server Development/Script Includes/MultiDeveloperWillWorkOnIt.script.js
@@ -2,6 +2,10 @@ var MultiDeveloperWillWorkOnIt = Class.create();
 MultiDeveloperWillWorkOnIt.prototype = {
 	initialize: function () {
 		// my comments
+		// Double added comments for feature_branch2
+	},
+	myAdd: function(){
+
 	},
 
 	type: "MultiDeveloperWillWorkOnIt"
